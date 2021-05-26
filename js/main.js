992ca9b484e3fbab0fd5fb431d1f25ba7a4f8b68
Serg <sergioleningrad@gmail.com>
2021-05-26 22:14:26 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let j = "Dude";
+
+const obj = {
+    name:"Serg",
+    age:25
+};
+
+let arr = ["first", "second"];
+
+let un = BigInt(Number.MAX_SAFE_INTEGER);
+
+console.log(un + 200n);
